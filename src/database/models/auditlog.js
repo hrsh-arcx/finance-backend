4be800ea01221af9b6d0fe1ccc52293d4f05286a
hrsh-arcx <harsh18goel@gmail.com>
2026-04-02 00:01:21 +0530

    feat: add audit log model for activity tracking

---
+++ src/database/models/auditlog.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class AuditLog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  AuditLog.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    action: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entityType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entityId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    metadata: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'AuditLog',
+  });
+  return AuditLog;
+};
