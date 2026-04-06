b8c7929c0ebcb023ea82dab417516fdd5a1091a7
hrsh-arcx <harsh18goel@gmail.com>
2026-04-06 10:03:05 +0530

    docs: add swagger api documentation

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ const morgan = require('morgan');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const {StatusCodes} = require('http-status-codes')
 const apiRouter = require('./routes')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
 
+const swaggerdocument = YAML.load('./src/docs/swagger.yaml')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerdocument));
 
 app.get('/health', (req, res) => {
   return res.status(StatusCodes.OK).json({
